73a1a1e7b7d15ec88018b7e3426687a10f1e1ffb
Calebagbakou <calebagbakou@gmail.com>
2026-09-05 06:06:45 +0100

    Add Supabase configuration and client initialization

---
+++ supabase/supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://umzjknzxigigyxxjnzlc.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_I5cnIZ01QEFC8QxkXySMTQ_EnrJJCD9";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
